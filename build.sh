b790ea353ef0e223a87dad349656474d849b5137
wbigger <wbigger@gmail.com>
2019-10-11 23:20:37 +0200

    fix summary

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ export MDBOOK_BUILD__BUILD_DIR="book-$CLASS"
 mdbook build || exit -1;
 echo "Generating zip file...";
 zip -r -X book.zip book-$CLASS > /dev/null || exit -1;
-echo "Copying book to docs...";
+echo "Copying book-$CLASS to docs...";
 rm -rf ./docs
 cp -r book-$CLASS docs
--- src/SUMMARY-1Ainf.md
+++ src/SUMMARY-1Ainf.md
@@ -1,5 +1,5 @@
 # Tavola dei contenuti
 
 - [Benvenuti](./README.md)
-- [HTML5 Walkthrough](./01-html5/README.md)
-  - [CSS](./01-html5/02-css.md)
+- [Data Link](./01-switch/README.md)
+- [Network](./02-network/README.md)
